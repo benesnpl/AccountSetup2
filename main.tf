2f0a8c1c18829b8ac7e3479156686fae87d1fec2
Benelab <78307945+benesnpl@users.noreply.github.com>
2022-05-01 07:37:09 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "aws_vpc" "main_vpc" {
   enable_dns_hostnames             	= var.enable_dns_hostnames
   enable_dns_support              	= var.enable_dns_support
   tags = {
-    Name = "TerraVPC"
+    Name = "Test_VPC"
   }
 }
 
