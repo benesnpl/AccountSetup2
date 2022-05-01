5e11f113275be34395d6452f05740c03129d69a6
Benelab <78307945+benesnpl@users.noreply.github.com>
2022-05-01 05:19:30 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,44 @@
+# VPC
+resource "aws_vpc" "terra_vpc" {
+  cidr_block       					= var.vpc_cidr
+  instance_tenancy 					= var.instance_tenancy
+  enable_dns_hostnames             	= var.enable_dns_hostnames
+  enable_dns_support              	= var.enable_dns_support
+  tags = {
+    Name = "TerraVPC"
+  }
+}
 
+resource "aws_internet_gateway" "terra_igw" {
+  vpc_id = aws_vpc.terra_vpc.id
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_subnet" "public" {
+  count = length(var.subnets_cidr)
+  vpc_id = aws_vpc.terra_vpc.id
+  cidr_block = element(var.subnets_cidr,count.index)
+  availability_zone = element(var.azs,count.index)
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "Subnet-${count.index+1}"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.terra_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.terra_igw.id
+  }
+  tags = {
+    Name = "publicRouteTable"
+  }
+  
+resource "aws_route_table_association" "a" {
+  count = length(var.subnets_cidr)
+  subnet_id      = element(aws_subnet.public.*.id,count.index)
+  route_table_id = aws_route_table.public_rt.id
+}
